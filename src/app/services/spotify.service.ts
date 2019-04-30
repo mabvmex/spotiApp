495b932ae441f3ecd0e9094bf4b427e49649fcce
mabvmex <mabvmex@gmail.com>
2019-04-30 14:57:58 -0500

    Componente y servicio de busqueda hecho

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,3 +1,4 @@
+
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
@@ -13,11 +14,16 @@ export class SpotifyService {
   getNewReleases() {
 
     const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQD2nj1oN3NqcoGrzOjtdlyY3p3ukGjgjkQjWFlXOCR8NoG4kjwMqRcz6CSHHq6kShsa2gfIvk6BQ8yThFpTNxn1K8ekdGzdrpAYoUtUpl5dWfM4KVUwdoyZhIJRbgrl9LyqHI1Wa5ZfXg' });
-
+      'Authorization': 'Bearer BQDDrvrp3GNgW6RzzRogoHKXysAjQ0cGyidHinW0_nffIStdc_ZZrDEfaxWDFp4PyzInkSiGke1uzhl17j6u7GtnsJv5qS5GAlSgHZTaeUyIM2h8sEG0qWPvi1LC0sGHAAtAM6G1sl8RBw'
+    });
     return this.http.get('https://api.spotify.com/v1/browse/new-releases', { headers });
-      // .subscribe((data: any) => {
-      //   console.log(data);
-      // });
   }
+
+  getArtist(termino: string) {
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer BQDDrvrp3GNgW6RzzRogoHKXysAjQ0cGyidHinW0_nffIStdc_ZZrDEfaxWDFp4PyzInkSiGke1uzhl17j6u7GtnsJv5qS5GAlSgHZTaeUyIM2h8sEG0qWPvi1LC0sGHAAtAM6G1sl8RBw'
+    });
+    return this.http.get(`https://api.spotify.com/v1/search?q=${termino}&type=artist`, { headers });
+  }
+
 }
